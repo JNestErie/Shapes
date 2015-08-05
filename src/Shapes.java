2b6d6f4eacda0643a987dff5eb420c3179447e5b
JNestErie <jnesterie@outlook.com>
2015-08-05 11:26:05 -0700

    Updated @ 11:25 AM

--- src/Shapes.java
+++ src/Shapes.java
@@ -1,42 +1,54 @@
 import java.util.Scanner;
 
 /**
- * Author: Jackson       				SE Project 1
+ * Author: Jackson                      SE Project 1
  * Due: August 10						30 Points
  * Description: Program will calculate volume or surface area dependent upon user input.
  * Input: length, width, height, or radius dependent upon the object.
- * Assumptions: User needs massive guidance to successfully navigate the program.
+ * Assumptions: User needs guidance to successfully navigate the program.
  */
 public class Shapes {
 
-	public static Scanner keyboardScanner = new Scanner(System.in); //Defined global - will be used throughout program.
+	public static Scanner keyboardScanner = new Scanner(System.in); //Used throughout program
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello there! I'll be computing surface area or volume for you today!");
-		System.out.println("Please select which you'd like to compute. Enter 'SA'for surface area  or 'V' for volume.");
+		/*
+		 * Here we'll define a ton of variables.
+		 */
+		double userWidth = 0;
+		double userLength = 0;
 		
-		String userChoice = keyboardScanner.next(); //Allow any input BUT if it isn't SA or V, go to the else statement.
+		System.out.println("Hello there! I'll be computing surface area " +
+                        "or volume for you today!");
+		System.out.print("Please select which you'd like to compute." +
+                       " Enter 'SA'for surface area  or 'V' for volume: ");
 		
-		
-		if (userChoice == "SA") {
-			System.out.println("Great! Now select your object. 'RS' for a rectangular solid, 'C' for a cylinder ");
-			System.out.println("'S' for a sphere or 'C' for a cone");
-			//TODO: get values from user.
-			String objectChoice = keyboardScanner.next();
-			System.out.println(objectChoice);
+		 String userChoice = keyboardScanner.nextLine(); //Allow user input.
+                
+                if (userChoice.equals("SA")) {
+                    System.out.println("Awesome. Surface area has been chosen.");
+                    //Get length.
+                    System.out.print("Please enter the length: ");
+                    userLength = keyboardScanner.nextDouble();
+                    //Get width now.
+                    System.out.println("Thanks. Now input the width");
+                    userWidth = keyboardScanner.nextDouble();
+                }
+                
+                
+                else if (userChoice.equals("V"))  {
+                    System.out.println("Output for V");
+                }
+
+                else {
+                	System.out.println("You've submitted something that isn't valid. " + 
+                					   "Please restart the application.");
+                }
+
+                
+                                                            
 			
-		}
-		
-		
-		if (userChoice == "V") {
-			System.out.println("Volume, awesome.");
-		}
-		
-		else {
-			System.out.println("Allegedly Java needs an else.");
-		}
-				
 	}
 
 }
